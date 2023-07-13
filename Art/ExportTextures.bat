57e529079a56e79d73aba24b415ffd85f35d54d1
mucrow <120820315+mucrow@users.noreply.github.com>
2023-07-13 18:31:56 -0400

    Create git repository

---
+++ Art/ExportTextures.bat
@@ -0,0 +1,1 @@
+aseprite --batch --split-slices --layer "Main" .\Textures.aseprite --save-as ..\Citrine\Assets\Textures\{slice}.png
---
+++ Citrine/Assets/Scripts/PrintHello.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Citrine {
+  public class PrintHello: MonoBehaviour {
+    void Start() {
+      Debug.Log("Hello.");
+    }
+  }
+}
